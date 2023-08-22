e034ffe6d48d5dc49ce205c5b9a15f7463c79883
magm3333 <magm3333@gmail.com>
2023-08-22 18:24:39 -0300

    Inicial

---
+++ src/main/java/ar/edu/iua/iw3/backend/model/Product.java
@@ -0,0 +1,16 @@
+package ar.edu.iua.iw3.backend.model;
+
+
+@Getter
+public class Product {
+
+
+	private long id;
+	
+	private String product;
+	
+	private boolean stock=true;
+	
+	private double price;
+	
+}
